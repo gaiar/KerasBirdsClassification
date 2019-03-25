d35705490734462d8296780a1bfe319da5c93a14
gaiar <gaiar@baimuratov.ru>
2019-03-25 08:43:49 +0100

    WIP :: Add validation images from berlin.de

--- datasets/download_images.py
+++ datasets/download_images.py
@@ -2,35 +2,49 @@ from google_images_download import google_images_download
 import random 
 import csv
 
-arguments = dict()
-arguments["limit"] = 100
-arguments["print_urls"] = False
-arguments["format"] = "jpg"
-arguments["size"] = ">400*300"
-arguments["color_type"] = "full-color"
-arguments["output_directory"] = "data"
-#arguments["suffix_keywords"] = "winter,sommer,wald"
-#arguments["language"] = "German"
-#arguments["usage_rights"] = "labeled-for-reuse"
 
-proxies_file = open("proxies.lst", "r+")
+def download_from_google():
 
-proxies = proxies_file.readlines()
+    #Prepare config for google_images_download
+    arguments = dict()
+    arguments["limit"] = 100
+    arguments["print_urls"] = False
+    arguments["format"] = "jpg"
+    arguments["size"] = ">400*300"
+    arguments["color_type"] = "full-color"
+    arguments["output_directory"] = "data"
 
-with open('berlin-birds.csv', newline='') as csvfile:
-    birdreader = csv.DictReader(csvfile, delimiter=',')
-    for row in birdreader:
-        # print(row[0])
-        response = google_images_download.googleimagesdownload()
-        keywords = "{0} OR {1} {2}".format(row["name"], row["latin_name"], "-stock")
-        prefix = "{0}{1}".format(row["name"].lower().replace(" ", "_"), "_")
-        arguments["keywords"] = keywords
-        arguments["prefix"] = prefix
-        arguments["image_directory"] = row["name"]
-        arguments["proxy"] = random.choice(proxies).strip()
-        print("Downloading {}".format(arguments))
-        try:
-            response.download(arguments)
-        except Exception:
+    #Extra fine-tuning if needed
+    
+    #arguments["suffix_keywords"] = "winter,sommer,wald"
+    #arguments["language"] = "German"
+    #arguments["usage_rights"] = "labeled-for-reuse"
+
+    #Get proxies 
+    proxies_file = open("proxies.lst", "r+")
+    proxies = proxies_file.readlines()
+
+    with open('berlin-birds.csv', newline='') as csvfile:
+        birdreader = csv.DictReader(csvfile, delimiter=',')
+        for row in birdreader:
+            # print(row[0])
+            response = google_images_download.googleimagesdownload()
+            keywords = "{0} OR {1} {2}".format(row["name"], row["latin_name"], "-stock")
+            prefix = "{0}{1}".format(row["name"].lower().replace(" ", "_"), "_")
+            arguments["keywords"] = keywords
+            arguments["prefix"] = prefix
+            arguments["image_directory"] = row["name"]
             arguments["proxy"] = random.choice(proxies).strip()
-            response.download(arguments)
+            print("Downloading {}".format(arguments))
+            try:
+                response.download(arguments)
+            except Exception:
+                arguments["proxy"] = random.choice(proxies).strip()
+                response.download(arguments)
+
+    proxies_file.close()
+
+def dowload_original_files():
+    with open('berlin-birds.csv', newline='') as csvfile:
+        birdreader = csv.DictReader(csvfile, delimiter=',')
+    
